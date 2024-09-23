6a92b4f330c534ebe718671243b2329f110f44fa
Manav Malhotra <roughsheet2002@gmail.com>
2024-09-24 01:40:55 +0530

    css change

--- src/features/shortListedCV/ShortListCvRow.jsx
+++ src/features/shortListedCV/ShortListCvRow.jsx
@@ -26,7 +26,7 @@ export const ShortListCvRow = ({ data }) => (
     </div>
     <button className="bg-gradient-to-br from-blue-500 to-blue-700 text-white rounded-xl text-nowrap flex items-center justify-center p-2 px-5 space-x-2 h-12 w-[30%] flex-shrink-0">
       <Download className="w-4 h-4" />
-      <span className="text-sm">Download for 30 credits</span>
+      <span className="md:text-sm text-xs">Download for 30 credits</span>
     </button>
     <button className="rounded w-4 h-4 flex-shrink-0 bg-blue-700 text-white flex items-center justify-center">
       -
